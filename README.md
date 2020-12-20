6166a0d096832ab4b8f1b5f05b38c4e9e2711952
krishnakalavadia <kkalavad@uwaterloo.ca>
2020-12-20 16:26:22 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Face Detection and Image Processing
+
+Introductory project to learn basic openCV and image processing. <br>
+
+Created a program that detects faces, eyes and smiles and outlines them. 
+After detecting faces, each face is sharpened using a common image sharpening convolution kernel 
+and the background is smoothed using a Gaussian Blur.
+
+## Screenshots
+
+ ![Image](screenshots/example_img.png "Main Page")
+ (Not the best picture of my Mom and I)
+## Installation on CLion
+To install and run this Project on CLion:
+1. Clone the repository 
+2. Install the latest version of OpenCV
+3. Open CLion, and select "Open or Import" and select the cloned repository 
+4. Right click "CMakeLists.txt" and select "Reload CMake project" to ensure OpenCV is found
+5. Go to Run > Edit Configurations > Ensure Working Directory is selected as the current directory so the program can find the three Haar Cascade files included in the repository 
+    5. The three Haar Cascade files were taken from OpenCV's collection of Haar Cascade files so if you wish to use a different Haar Cascade from there such as "haarcascade_frontalface_alt.xml" then include the file path when you load the Haar Cascade 
+6. Run project
+## Usage
+When the project is run, it will open your web-cam, start detecting faces and filtering the image <br>
+
+To quit the program press any key and it will terminate.
+
+
+## References and Acknowledgment
+[1] [OpenCV C++ Face Detection](https://www.geeksforgeeks.org/opencv-c-program-face-detection/) <br>
+[2] [Image Filtering](https://docs.opencv.org/master/d4/d86/group__imgproc__filter.html#gaabe8c836e97159a9193fb0b11ac52cf1) <br>
+[3] [Cascade Classifier](https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html) <br>
+[4] [Drawing Functions](https://docs.opencv.org/master/d6/d6e/group__imgproc__draw.html) <br>
+[5] [OpenCV: Filters & Arithmetic Operations](https://medium.com/@almutawakel.ali/opencv-filters-arithmetic-operations-2f4ff236d6aa) <br>
+[6] [Verify Rect Object is Inside Mat Object ](https://stackoverflow.com/questions/29120231/how-to-verify-if-rect-is-inside-cvmat-in-opencv) (used in line 96)<br>
+
+
+
---
+++ face_detection.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+/**
+   Detects faces, eyes and smiles, outlines them, blurs background and sharpens each face
+
+   @param image - Mat object that holds a frame from the web-cam
+   @param gray_scale_image Mat object that holds the grey scale converted frame from web-cam
+   @param face_cascade Haar Cascade face classifier
+   @param eye_cascade Haar Cascade eye classifier
+   @param smile_cascade Haar Cascade smile classifier
+   @param scaling_factor Scaling factor used to reduce computational load
+   @param color_array Array of various colors
+   @return void
+*/
+void detect_and_process(Mat& image, const Mat& gray_scale_image,
+                        CascadeClassifier& face_cascade,
+                        CascadeClassifier& eye_cascade,
+                        CascadeClassifier& smile_cascade,
+                        double scaling_factor, Scalar color_array[]);
+
+int main() {
+    cout << "Starting detection program" << endl;
+
+    double scaling_factor = 2; // Used for scaling image
+    CascadeClassifier face_cascade; // Face Haar Cascade
+    CascadeClassifier eye_cascade; // Eye Haar Cascade
+    CascadeClassifier smile_cascade; // Smile Haar Cascade
+
+    face_cascade.load("HaarCascade_Models/haarcascade_frontalface_default.xml");
+    eye_cascade.load("HaarCascade_Models/haarcascade_eye_tree_eyeglasses.xml");
+    smile_cascade.load("HaarCascade_Models/haarcascade_smile.xml");
+
+    Scalar red = Scalar(0, 0, 255);
+    Scalar blue = Scalar(255, 0, 0);
+    Scalar green = Scalar(0, 255, 0);
+    Scalar white = Scalar(255, 255, 255);
+    Scalar colors [] = {red, blue, green, white};
+
+    // Opens '0th' web cam on the system
+    VideoCapture capture(0);
+
+    if (!capture.isOpened()) {
+        return -1;
+    }
+
+    while (true) {
+        Mat frame;  // Holds each frame from the capture
+        Mat gray_scale_frame; // Holds grey scale converted frame
+        capture >> frame;
+        // Convert frame to grayscale
+        cvtColor(frame, gray_scale_frame, COLOR_BGR2GRAY);
+        // Resize width and height to reduce computational load and decrease lag on web cam
+        resize(gray_scale_frame, gray_scale_frame, Size(gray_scale_frame.size().width / scaling_factor,
+                                                        gray_scale_frame.size().height / scaling_factor));
+        // Detect faces, blur background and sharpen faces
+        detect_and_process(frame, gray_scale_frame, face_cascade, eye_cascade,
+                           smile_cascade, scaling_factor, colors);
+
+        putText(frame,"Face Detection", Point(5, 20),FONT_HERSHEY_COMPLEX_SMALL,1.0,
+                colors[1],1
+        );
+
+
+        // show the frame which now contains outlines faces, text and the blurred background
+        imshow("Camera Frame", frame);
+        // if key is pressed break the loop
+        if (waitKey(10) > 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+
+void detect_and_process(Mat& image, const Mat& gray_scale_image,
+                        CascadeClassifier& face_cascade,
+                        CascadeClassifier& eye_cascade,
+                        CascadeClassifier& smile_cascade,
+                        double scaling_factor, Scalar color_array[]){
+
+    vector <Rect> faces; // holds detected faces
+    vector<Mat> sub_images; // holds Mat objects that contain only the face not background
+
+    // Use the Haar Cascade classifier to find faces in the grey_scale frame
+    face_cascade.detectMultiScale(gray_scale_image, faces, 1.1, 5, 0, Size(30, 30));
+
+    for (size_t i = 0; i < faces.size(); i++) {
+
+        Rect rect_with_face(cvRound(faces[i].x* scaling_factor),
+                            cvRound(faces[i].y* scaling_factor),
+                            faces[i].width* scaling_factor,
+                            faces[i].height* scaling_factor);
+
+        // Prevents crashing if the rect_with_face is outside the bounds of the image itself
+        //      The statment 'bool is_rect_inside' was taken from stackoverflow answer listed below
+        //      https://stackoverflow.com/questions/29120231/how-to-verify-if-rect-is-inside-cvmat-in-opencv
+        bool is_rect_inside = (rect_with_face & Rect(0, 0, image.cols, image.rows)) == rect_with_face;
+        if (!is_rect_inside) {
+            cout << "Outside Range\n";
+            continue;
+        }
+
+        Mat face_sub_img; // holds ROI of image that contains the face
+        Mat sharpening_kernel; // holds kernel used to sharpen image
+
+        // Isolate the area with the face and sharpen it using a common sharpening kernel
+        face_sub_img = image(rect_with_face);
+        sharpening_kernel = (Mat_<double>(3, 3) << -1, -1, -1, -1, 9, -1, -1, -1, -1);
+        filter2D(face_sub_img, face_sub_img, -1, sharpening_kernel);
+        sub_images.push_back(face_sub_img);
+
+        // Draw rectangle for the face
+        rectangle(image, rect_with_face, color_array[0], 3, 8, 0);
+
+        // Detect eyes
+        vector<Rect> eyes;
+        eye_cascade.detectMultiScale(gray_scale_image, eyes, 1.1, 5, 0, Size(30, 30));
+
+        for (size_t j = 0; j < eyes.size(); j++) {
+            // Draw a rect around each eye in blue
+            rectangle(image, Point(cvRound(eyes[j].x * scaling_factor),
+                                   cvRound(eyes[j].y * scaling_factor)),
+                      Point(cvRound((eyes[j].x + eyes[j].width) * scaling_factor),
+                            cvRound((eyes[j].y + eyes[j].height) * scaling_factor)),
+                      color_array[1], 3, 8, 0);
+        }
+
+        // Detect smiles only in the area that the face was detected
+        Mat smile_frame;
+        Mat gray_scale_smile_frame;
+        smile_frame = image(rect_with_face);
+        // Convert frame to grayscale
+        cvtColor(smile_frame, gray_scale_smile_frame, COLOR_BGR2GRAY);
+        vector <Rect> smiles;
+        // Increase the minNeighbers for less false positives
+        smile_cascade.detectMultiScale(gray_scale_smile_frame, smiles, 1.8, 20, 0,
+                                       Size(30, 30));
+
+        Rect rect_for_label(cvRound(faces[i].x * scaling_factor),cvRound(faces[i].y * scaling_factor),
+                            120 * scaling_factor, 15 * scaling_factor);
+        rectangle(image,rect_for_label,color_array[0],-1,8,0);
+        // Flag whether or not a smile was found
+        if (!smiles.empty()) {
+            putText(image, "Smile Detected", Point(faces[i].x * scaling_factor + 3, faces[i].y * scaling_factor + 20),
+                    cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0,
+                    color_array[2], 1
+            );
+        }
+        else {
+            putText(image, "No Smile Detected", Point(faces[i].x * scaling_factor + 3, faces[i].y * scaling_factor + 20),
+                    cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0,
+                    color_array[3], 1);
+        }
+
+    }
+
+    // Blur the background but keep each face there
+    Mat blurred_background; // holds background with Gaussian Blur applied
+    blurred_background = image.clone();
+    GaussianBlur(blurred_background, blurred_background, Size(0, 0), 10, 10);
+    for (size_t i = 0; i < faces.size(); i++) {
+        Rect rect_with_face(cvRound(faces[i].x* scaling_factor),
+                            cvRound(faces[i].y* scaling_factor),
+                            faces[i].width* scaling_factor,
+                            faces[i].height* scaling_factor);
+        sub_images[i].copyTo(blurred_background(rect_with_face));
+    }
+
+    blurred_background.copyTo(image);
+
+    string face_count_display = "Number of faces found: " + to_string(faces.size());
+    putText(image, face_count_display, Point(5, 40), FONT_HERSHEY_COMPLEX_SMALL,1.0,
+            color_array[1],1
+    );
+}
